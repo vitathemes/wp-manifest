c389ffe17f1b4e6d15285a4d1be7740bc501e721
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2020-09-14 13:35:48 +0430

    Implement 404 page

--- 404.php
+++ 404.php
@@ -6,13 +6,20 @@
  *
  */
 
-get_header();
+wp_head();
 ?>
-    <section class="lost-container u-margin-bottom-larger-m u-margin-bottom-larger">
+<header class="c-header c-header__404">
+    <div class="c-header__main">
+        <h1 class="c-header__site-title">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+        </h1>
+    </div>
+</header>
+    <section class="lost-container not-found u-margin-bottom-larger-m u-margin-bottom-larger">
         <div class="o-wrapper">
-            <h2><?php _e('Page Not Found - 404' , 'wp-manifest'); ?></h2>
-            <p><?php _e('This page not found (deleted or never exists). try a phrase in search box or back to home and start again.' , 'wp-manifest'); ?></p>
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php _e('Take me home!' , 'wp-manifest'); ?></a>
+            <h2 class="big-title"><?php _e('404' , 'wp-manifest'); ?></h2>
+            <p><?php _e('Oops! we are sorry, but the page you requested was not found. You can search or return to home.' , 'wp-manifest'); ?></p>
+            <a class="c-btn c-btn--primary" href="<?php echo esc_url( home_url( '/' ) ); ?>"><span class="dashicons dashicons-arrow-left-alt"></span> <?php _e('Go Home' , 'wp-manifest'); ?></a>
             <br>
             <br>
 	        <div class="s-widget s-widget--search-form">
@@ -21,4 +28,4 @@ get_header();
         </div>
     </section>
 <?php
-get_footer();
+wp_footer();
