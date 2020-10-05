e23e847a43db3751af29da718d46a4f9fd1a4d9c
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2020-10-05 09:59:37 +0330

    Fix minor issues

--- template-parts/components/info.php
+++ template-parts/components/info.php
@@ -2,17 +2,15 @@
     <div class="o-wrapper">
         <div class="c-info__main">
             <div class="c-info__col o-col o-col--1/2">
-                <h3 class="c-info__col__title">
-					<?php echo esc_html( get_theme_mod( 'homepage_info_left' , __('Full-time UI/UX designer Head of Design at VeronaLabs.com', 'wp-manifest') ) ); ?>
-                </h3>
+                <div class="c-info__col__title h3">
+					<?php echo wp_kses_post( get_theme_mod( 'homepage_info_left' , __('Full-time UI/UX designer Head of Design at VeronaLabs.com', 'wp-manifest') ) ); ?>
+                </div>
                 <a class="c-info__col__link" href="<?php echo esc_url(get_theme_mod( 'homepage_info_left_link', '/about')); ?>">
                     <span class="u-vertical-middle dashicons dashicons-arrow-right-alt"></span>
                 </a>
             </div>
             <div class="c-info__col o-col o-col--1/2 u-margin-none c-info__col--desc">
-                <p class="u-margin-none">
-					<?php echo esc_html( get_theme_mod( 'homepage_info_right' , __('We work with clients around the world from our headquarters in Charleston, South Carolina. We focus on naming, branding, brand narratives, website design and development, and brand experiences.', 'wp-manifest') ) ); ?>
-                </p>
+					<?php echo wp_kses_post( get_theme_mod( 'homepage_info_right' , __('We work with clients around the world from our headquarters in Charleston, South Carolina. We focus on naming, branding, brand narratives, website design and development, and brand experiences.', 'wp-manifest') ) ); ?>
             </div>
         </div>
     </div>
